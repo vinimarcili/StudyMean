fb54ceea77afd4d478c1f109295c40a85794ed35
Vinícius Marcili <vinipetter@gmail.com>
2016-12-17 14:29:50 -0200

    Conteudo dinamico na página principal da lista de locais

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -1,6 +1,50 @@
 /* GET 'home' page */
 module.exports.homeList = function (req, res) {
-    res.render('locations-list', {title: 'Home'});
+    res.render('locations-list', {
+        title: 'Loc8r - find a place to work with wifi',
+        pageHeader: {
+            title: 'Loc8r',
+            strapline: 'Find places to work with wifi near you!'
+        },
+        sidebar :   "Looking for wifi and a seat? " +
+                    "Loc8r helps you find places to work when out and about. " +
+                    "Perhaps with coffee, cake or a pint? " +
+                    "Let Loc8r help you find place you're looking for.",
+        locations: [
+            {
+                name: 'Starcups',
+                address: '125 DESCUBRA, Reading, RG6 1PS',
+                rating: 3,
+                facilities: [
+                    'Hot Drinks',
+                    'Food',
+                    'Premium Wifi'
+                ],
+                distance: '100m'
+            },
+            {
+                name: 'Cafe Hero',
+                address: 'Próximo ao MIM ACHER',
+                rating: 5,
+                facilities: [
+                    'Hot Drinks',
+                    'Didico',
+                    'Premium Wifi'
+                ],
+                distance: '200m'
+            },
+            {
+                name: 'Burger Queen',
+                address: 'Casa do caralho',
+                rating: 2,
+                facilities: [
+                    'Hot Drinks',
+                    'Didico'
+                ],
+                distance: '250m'
+            }
+        ]
+    });
 };
 
 /* GET 'Location info' page */
