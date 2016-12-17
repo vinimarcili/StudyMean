ffcccc6650acb2dc9d273086e7955da44b16f714
Vinícius Marcili <vinipetter@gmail.com>
2016-12-17 15:06:40 -0200

    Página sobre

--- app_server/controllers/others.js
+++ app_server/controllers/others.js
@@ -1,4 +1,7 @@
 /* GET 'about' page */
 module.exports.about = function (req, res) {
-  res.render('generic-text', {title: 'About'});
+  res.render('generic-text', {
+    title: 'About Loc8r',
+    content: "Aqui é Body Builder Ipsum PORRA! Bora caralho, você quer ver essa porra velho. Vo derrubar tudo essas árvore do parque ibirapuera. Ó o homem ali porra!, é 13 porra! Aqui é bodybuilder porra! Que não vai dá rapaiz, não vai dá essa porra. É verão o ano todo vem cumpadi. \n\nEita porra!, tá saindo da jaula o monstro! Tá comigo porra. Ó o homem ali porra!, é 13 porra! Vem porra! Vamo monstro! É 37 anos caralho! \n\nEle tá olhando pra gente. Que não vai dá rapaiz, não vai dá essa porra. Birl! AHHHHHHHHHHHHHHHHHHHHHH..., porra! Eita porra!, tá saindo da jaula o monstro! Vem porra!"
+  });
 };
